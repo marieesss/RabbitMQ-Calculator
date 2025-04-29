85ce138725b631987265ed88e2420a7dc0d9296f
Marie <mje.espinosa@gmail.com>
2025-04-29 16:22:42 +0200

    change routing key depending request

--- producer/app.py
+++ producer/app.py
@@ -9,12 +9,12 @@ import random
 
 connection = None
 channel = None
-operation="sum"
+routing_key="operation.sum.*"
 
 def init_rabbitmq():
     global connection, channel
     try:
-        exchange_name = "topic_exchange"
+        exchange_name = "calc_exchange"
         # Connection à RabbitMQ
         connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=pika.PlainCredentials("admin", "admin"),))
         channel = connection.channel()
@@ -28,21 +28,22 @@ def init_rabbitmq():
         app.logger.error(f"Erreur lors de l'initialisation de RabbitMQ : {str(e)}")
         connection, channel = None, None
 
-def checker():
+def send_numbers():
     # Random numbers
     num1 = random.randint(0, 100)
     num2 = random.randint(0, 100)
     app.logger.info('Démarrage de la consommation RabbitMQ... %s and %s', num1, num2)
 
+    # If channel exists, send number to routing key choosen
     if channel is not None:
-
         message=json.dumps({"num1" : num1, "num2" : num2})
-        channel.basic_publish(exchange="topic_exchange", routing_key=operation, body=message)
-        app.logger.info('topic envoyé')
+        channel.basic_publish(exchange="topic_exchange", routing_key=routing_key, body=message)
+        app.logger.info(routing_key)
+        app.logger.info(message)
 
 def checker_thread():
     while True:
-        checker()
+        send_numbers()
         time.sleep(5)
 
 app = Flask(__name__)
@@ -52,10 +53,25 @@ app.logger.setLevel(logging.DEBUG)
 
 @app.route('/',  methods=['POST'])
 def get_operations():
+    global routing_key
     data = request.get_json()
     operation = data['operation']
 
-    return jsonify({"operation choosed": operation})
+    approuved_operations = [{"key" : "addition", "routing_key": "operation.add"}, {"key" : "soustraction", "routing_key": "operation.sub"}, {"key" : "division", "routing_key": "operation.div"}]
+
+    # Chercher l'opération dans la liste
+    operation_found = None
+    for op in approuved_operations:
+        if op['key'] == operation:
+            # Change global routine key
+            routing_key = op.get('routing_key')
+            operation_found = op
+            break 
+
+    if operation_found:
+        return jsonify({"message": f"Operation '{operation}' found!", "routing_key": routing_key})
+    else:
+        return jsonify({"message": f"Operation '{operation}' not found!"}), 404
 
 
 if __name__ == '__main__':
